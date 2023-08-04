5b879b3d46edf799583c573b220694411f936d37
trietlv0910 <trietlv0910@gmail.com>
2023-08-04 23:04:59 +0700

    CRIM-11

---
+++ app/src/main/java/vn/android600/criminalintent/ui/crimes/CrimeListViewModel.kt
@@ -0,0 +1,16 @@
+package vn.android600.criminalintent.ui.crimes
+
+import androidx.lifecycle.ViewModel
+import vn.android600.criminalintent.models.Crime
+
+class CrimeListViewModel : ViewModel(){
+
+    val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 0 until 100){
+            val crimeTmp = Crime(title = "Crime #$i")
+            crimes += crimeTmp
+        }
+    }
+}
