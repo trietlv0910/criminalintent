6b974c7f014028ed36113ec1dcf4fa4bc90aeb57
trietlv0910 <trietlv0910@gmail.com>
2023-08-04 21:07:07 +0700

    CRM-6

--- app/src/main/java/vn/android600/criminalintent/MainActivity.kt
+++ app/src/main/java/vn/android600/criminalintent/MainActivity.kt
@@ -2,10 +2,20 @@ package vn.android600.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun loadFragment(fragment : Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
