66506e60fb07a2e2dbce2b2b413afb04a141d9e5
trietlv0910 <trietlv0910@gmail.com>
2023-08-04 22:02:50 +0700

    CRIM-9

--- app/src/main/java/vn/android600/criminalintent/models/Crime.kt
+++ app/src/main/java/vn/android600/criminalintent/models/Crime.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 
 data class Crime(
         val id : UUID = UUID.randomUUID(),
-        var title : String,
+        var title : String = "",
         val date : Date = Date(),
         var isSolved : Boolean = false
     )
--- app/src/main/java/vn/android600/criminalintent/ui/MainActivity.kt
+++ app/src/main/java/vn/android600/criminalintent/ui/MainActivity.kt
@@ -1,14 +1,20 @@
-package vn.android600.criminalintent
+package vn.android600.criminalintent.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import vn.android600.criminalintent.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadDefaultFragment()
+    }
 
+    private fun loadDefaultFragment(){
+        val fragment = CrimeFragment.instance()
+        loadFragment(fragment)
     }
 
     private fun loadFragment(fragment : Fragment){
