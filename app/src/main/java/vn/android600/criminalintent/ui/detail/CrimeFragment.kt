18f191022e5ddbddb16f24f09c1b4b00296d90e8
trietlv0910 <trietlv0910@gmail.com>
2023-09-16 16:41:26 +0700

    implicitintent

--- app/src/main/java/vn/android600/criminalintent/ui/detail/CrimeFragment.kt
+++ app/src/main/java/vn/android600/criminalintent/ui/detail/CrimeFragment.kt
@@ -1,6 +1,11 @@
 package vn.android600.criminalintent.ui.detail
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.ContactsContract.Contacts
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
@@ -10,6 +15,7 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
@@ -27,11 +33,24 @@ import java.util.UUID
 
 class CrimeFragment : Fragment() {
 
+    private val selectSuspect = registerForActivityResult(
+        ActivityResultContracts.PickContact()
+    ){
+        it?.let {
+            parseContactSelection(it)
+        }
+    }
+
     private lateinit var root : View
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox : CheckBox
     private lateinit var titleEditText: EditText
+    private lateinit var suspectButton: Button
+    private lateinit var reportButton: Button
+
+
     private lateinit var crime: Crime
+
 
     private var isUpdateMode = true
 
@@ -110,7 +129,20 @@ class CrimeFragment : Fragment() {
                     null
                 )
             }
+        }
 
+        reportButton.setOnClickListener {
+            Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, this@CrimeFragment.crime.title)
+                putExtra(Intent.EXTRA_SUBJECT, "PH LOC")
+            }.also {
+                val chooserIntent = Intent.createChooser(it, getString(R.string.send_crime_report))
+                startActivity(chooserIntent)
+            }
+        }
+        suspectButton.setOnClickListener {
+            selectSuspect.launch(null)
         }
     }
 
@@ -135,6 +167,25 @@ class CrimeFragment : Fragment() {
         dateButton = root.findViewById(R.id.date_btn)
         solvedCheckBox = root.findViewById(R.id.solved_cb)
         titleEditText = root.findViewById(R.id.crime_title_edt)
+        suspectButton = root.findViewById(R.id.suspect_btn)
+        reportButton = root.findViewById(R.id.report_btn)
+    }
+
+    @SuppressLint("Recycle")
+    private fun parseContactSelection(contactUri : Uri){
+        val queryFields = arrayOf(Contacts.DISPLAY_NAME, ContactsContract.CommonDataKinds.Email.DISPLAY_NAME)
+
+        // create cursorloader
+        val queryCursor = requireActivity()
+            .contentResolver
+            .query(contactUri, queryFields, null, null, null)
+
+        queryCursor?.use {
+            if (it.moveToFirst()){
+                val suspect = it.getString(1)
+                suspectButton.text = suspect
+            }
+        }
     }
 
     companion object{
